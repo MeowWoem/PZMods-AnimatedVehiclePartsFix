382392a7898ec368738ba71c3c4ab3bde9dc89f2
Amenophis <l.stoller@outlook.fr>
2026-09-23 21:40:32 +0200

    Expose the class to Lua

--- Sources/Java/src/io/ameno/AVPF/AnimatedVehiclePartsFix.java
+++ Sources/Java/src/io/ameno/AVPF/AnimatedVehiclePartsFix.java
@@ -1,5 +1,8 @@
 package io.ameno.AVPF;
 
+import me.zed_0xff.zombie_buddy.Exposer;
+
+@Exposer.LuaClass
 public class AnimatedVehiclePartsFix {
 
     public static final boolean ZB_IS_ACTIVE = true;
